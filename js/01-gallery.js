88b42767bb4f9c2379bfb16c582b95316978ed5f
Olena Bondar <olena.bondar1060@gmail.com>
2022-10-28 13:15:41 +0300

    01 task

---
+++ js/01-gallery.js
@@ -0,0 +1,45 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const galleryRef = document.querySelector(".gallery");
+// створення розмітки
+const makeUpImageRef = (galleryItem) => {
+  const { preview, original, description } = galleryItem;
+
+  return `<div class="gallery__item "><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+};
+
+const makeImageGalleryMarkUp = galleryItems.map(makeUpImageRef).join("");
+
+galleryRef.insertAdjacentHTML("beforeend", makeImageGalleryMarkUp);
+
+// додаємо слухача подіі на галерею, отримуємо змінну, яка містить посилання на фото більшого формату
+galleryRef.addEventListener("click", onClickGetSourceImageGallery);
+let selectedImageSourse = null;
+
+function onClickGetSourceImageGallery(event) {
+  event.preventDefault();
+
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const currentActiveImage = document.querySelector(".gallery__image--active");
+
+  if (currentActiveImage) {
+    currentActiveImage.classList.remove("gallery__image--active");
+  }
+  const nextActiveImage = event.target;
+  nextActiveImage.classList.add("gallery__image--active");
+  selectedImageSourse = nextActiveImage.dataset.source;
+}
+
+galleryRef.onclick = () => {
+  basicLightbox
+    .create(
+      `
+		<img width="1280" height="852" src="${selectedImageSourse}">
+	`
+    )
+    .show();
+};
